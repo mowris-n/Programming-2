ea412f0356ff3d15774d0d248c0f12c28b921a65
mowris-n <mowris.n@stu.janesville.k12.wi.us>
2025-01-28 21:09:23 +0000

    Changes 1/28

---
+++ Hello.py
@@ -0,0 +1,29 @@
+print("Hello, world!")
+# This is a single line comment
+""" data types:
+int - Integer  (whole numbers)
+float - floating point number (number with decimal)
+str - String (any text, in quotes)
+chr - Character( single key board character, in single quotes)
+bool - Boolean( either True or False, only)
+"""
+
+""" Arithmetic Operators:
++   Add
+-   Subtract
+*   Multiply
+/   Divide
+//  Floor Divide (divide and round down to nearest int)
+%   Mod (Modulus/reminder from Int divison)
+**  Pow
+
+"""
+""" Assignment Operators:
+=   Value assignment (x = y)
++=  Add to (x += y is shorthand for x = x + y)
+-= Subtract from (shorthand for x = x - y)
+/=
+//=
+%=
+**=
+"""
